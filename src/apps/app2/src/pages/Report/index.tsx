7a93d026cc849a44200f5ddb18e69fac26d3453a
siyi <dajiadoujiaowosiyi@163.com>
2020-05-08 13:29:43 +0800

    子应用演示UIUI样式完成

--- src/apps/app2/src/pages/Report/index.tsx
+++ src/apps/app2/src/pages/Report/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { PageHeader } from 'antd';
+import { Chart, Geom, Tooltip, Legend } from 'bizcharts';
+
+const data = [
+    { genre: 'Sports', sold: 275, income: 2300 },
+    { genre: 'Strategy', sold: 115, income: 667 },
+    { genre: 'Action', sold: 120, income: 982 },
+    { genre: 'Shooter', sold: 350, income: 5271 },
+    { genre: 'Other', sold: 150, income: 3710 }
+  ];
+  
+  // 定义度量
+  const cols = {
+    sold: { alias: '销售量' },
+    genre: { alias: '游戏种类' }
+  };
 
 function Report() {
-    return (
-        <>Report</>
-    )
+  return (
+    <>
+    <PageHeader
+      ghost={false}
+      onBack={() => window.history.back()}
+      title="报表模块"
+      subTitle="独立开发独立部署"
+    ></PageHeader>
+     <Chart width={1000} height={400} data={data} scale={cols}>
+      <Legend position="top" dy={-20} />
+      <Tooltip />
+      <Geom type="interval" position="genre*sold" color="genre" />
+    </Chart>
+    </>
+  );
 }
 
 export default Report;
