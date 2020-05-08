d09e654b9cea49114d111d891e8ba9816f29b412
siyi <dajiadoujiaowosiyi@163.com>
2020-05-08 13:59:02 +0800

    子应用演示UIUI样式完成

--- src/apps/app1/src/pages/Marketing/index.tsx
+++ src/apps/app1/src/pages/Marketing/index.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { PageHeader, Button, Table } from 'antd';
+
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+];
+
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 
 function Marketing() {
-    return (
-        <>Marketing</>
-    )
+  return (
+    <>
+    <PageHeader
+      ghost={false}
+      onBack={() => window.history.back()}
+      title="营销模块"
+      subTitle="独立开发独立部署"
+      extra={[
+        <Button key="3">Operation</Button>,
+        <Button key="2">Operation</Button>,
+        <Button key="1" type="primary">
+          Primary
+        </Button>,
+      ]}
+    ></PageHeader>
+      <Table dataSource={dataSource} columns={columns} />
+    </>
+  );
 }
 
 export default Marketing;
